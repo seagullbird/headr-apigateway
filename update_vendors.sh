b8f9b4ee0a50f2a15a1a73d272461c9c634d3f9a
seagullbird <seagullbird823@gmail.com>
2018-04-18 11:10:59 +0800

    added update_vendors.sh; updated vendors

---
+++ update_vendors.sh
@@ -0,0 +1,5 @@
+dep ensure -update github.com/seagullbird/headr-common
+dep ensure -update github.com/seagullbird/headr-repoctl
+dep ensure -update github.com/seagullbird/headr-contentmgr
+dep ensure -update github.com/seagullbird/headr-sitemgr
+
--- vendor/github.com/seagullbird/headr-sitemgr/service/service.go
+++ vendor/github.com/seagullbird/headr-sitemgr/service/service.go
@@ -27,6 +27,7 @@ type Service interface {
 	UpdateConfig(ctx context.Context, siteID uint, config string) error
 	GetThemes(ctx context.Context, siteID uint) (string, error)
 	UpdateSiteTheme(ctx context.Context, siteID uint, theme string) error
+	PostAbout(ctx context.Context, siteID uint, content string) error
 }
 
 // New returns a basic Service with all of the expected middlewares wired in.
@@ -147,9 +148,17 @@ func (s basicService) GetThemes(ctx context.Context, siteID uint) (string, error
 	}
 
 	var (
-		theme1 = db.Theme{
-			Name:      "hugo-theme-cactus-plus",
-			ThumbNail: "https://d33wubrfki0l68.cloudfront.net/a765cc66e8105ff5527be210beeba91d1e561902/49ef9/hugo-theme-cactus-plus/tn-featured-hugo-theme-cactus-plus_hu5badda48e9249d0a91a08133ff38d1ab_2179148_768x512_fill_catmullrom_top_2.png",
+		themes = []db.Theme{
+			{
+				Name:      "hugo-theme-cactus-plus",
+				ThumbNail: "https://d33wubrfki0l68.cloudfront.net/a765cc66e8105ff5527be210beeba91d1e561902/49ef9/hugo-theme-cactus-plus/tn-featured-hugo-theme-cactus-plus_hu5badda48e9249d0a91a08133ff38d1ab_2179148_768x512_fill_catmullrom_top_2.png",
+				HomePage:  "https://themes.gohugo.io/hugo-theme-cactus-plus/",
+			},
+			{
+				Name:      "hyde-hyde",
+				ThumbNail: "https://d33wubrfki0l68.cloudfront.net/03edb0da66032d8d5831bebace3cc2c31af0a2f1/fdcbf/hyde-hyde/tn-featured-hyde-hyde_hu6fa6cbe8ad4d9c1dab6f62cb035f7b86_209417_768x512_fill_catmullrom_top_2.png",
+				HomePage:  "https://themes.gohugo.io/hyde-hyde/",
+			},
 		}
 	)
 
@@ -157,7 +166,7 @@ func (s basicService) GetThemes(ctx context.Context, siteID uint) (string, error
 		Themes       []db.Theme `json:"themes"`
 		CurrentTheme string     `json:"current_theme"`
 	}{
-		Themes:       []db.Theme{theme1},
+		Themes:       themes,
 		CurrentTheme: site.Theme,
 	}
 
@@ -174,6 +183,13 @@ func (s basicService) UpdateSiteTheme(ctx context.Context, siteID uint, theme st
 	if site.UserID != userID {
 		return ErrSiteNotFound
 	}
+
+	site.Theme = theme
+	err := s.store.PatchSite(site)
+	if err != nil {
+		return err
+	}
+
 	// https://stackoverflow.com/questions/11066946/partly-json-unmarshal-into-a-map-in-go?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 	originConfig, err := s.repoctlsvc.ReadConfig(ctx, siteID)
 	if err != nil {
@@ -193,4 +209,14 @@ func (s basicService) UpdateSiteTheme(ctx context.Context, siteID uint, theme st
 	newConfigRaw, err := json.Marshal(configMap)
 
 	return s.repoctlsvc.WriteConfig(ctx, siteID, string(newConfigRaw))
+}
+
+func (s basicService) PostAbout(ctx context.Context, siteID uint, content string) error {
+	userID := ctx.Value(jwt.JWTClaimsContextKey).(stdjwt.MapClaims)["sub"].(string)
+	site, _ := s.store.GetSite(siteID)
+	if site.UserID != userID {
+		return ErrSiteNotFound
+	}
+
+	return s.repoctlsvc.UpdateAbout(ctx, siteID, content)
 }
