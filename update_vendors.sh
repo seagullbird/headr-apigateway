9305cbaa409fb7f49cd5c2519870454256d0cc97
seagullbird <seagullbird823@gmail.com>
2018-04-18 15:52:05 +0800

    updated vendors

--- update_vendors.sh
+++ update_vendors.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+
 dep ensure -update github.com/seagullbird/headr-common
 dep ensure -update github.com/seagullbird/headr-repoctl
 dep ensure -update github.com/seagullbird/headr-contentmgr
 dep ensure -update github.com/seagullbird/headr-sitemgr
+git add .
+git cm -m 'updated vendors'
+git push
 
